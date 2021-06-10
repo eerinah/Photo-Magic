9766ebfe7a440c1524a0a89ac2d7767079edaa63
eerinah <83928244+eerinah@users.noreply.github.com>
2021-06-10 13:15:20 -0500

    Update PhotoMagicDeluxe.java

--- PhotoMagicDeluxe.java
+++ PhotoMagicDeluxe.java
@@ -1,4 +1,5 @@
 public class PhotoMagicDeluxe {
+   private static final String BASE64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
    public static String toBinary(int x, int len) {
       if (len > 0) {
          return String.format("%" + len + "s",
@@ -9,15 +10,16 @@ public class PhotoMagicDeluxe {
    
    public static String getBitKey(String alphaKey) { 
       String bitKey = "";
-      String base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
       for (int i = 0; i < alphaKey.length(); i++) { 
          int index = base64.indexOf(alphaKey.charAt(i));
-         bitKey += toBinary(index, 1);
+         int bitLength = 6;
+         bitKey += toBinary(index, bitLength);
       } 
       return bitKey; 
    }
    public static void main(String[] args) { 
-      String bitKey = getBitKey("OPENSESAME");
+      String passKey = "OPENSESAME";
+      String bitKey = getBitKey(passKey);
       Picture pic = new Picture("mystery.png");
       pic.show();
       LFSR lfsr = new LFSR(bitKey, 58);
